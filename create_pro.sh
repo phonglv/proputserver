288e764de9de1cd7b0f12c31a2a80187552aaa40
phonglv <levanphong1993@gmail.com>
2023-03-12 17:05:03 +0700

    Update create_pro.sh

--- create_pro.sh
+++ create_pro.sh
@@ -1,61 +1,141 @@
-  345  git clone https://github.com/DepartureLabsIC/non-fungible-token.git nft
-  346  cd nft
-  347  dfx start --background
-  348  dfx deploy --no-wallet
-  349  dfx identity get-principal
-  350  dfx canister call nft init '(vec {principal "oy7al-zoh7d-fk4pm-o4agp-imod5-j4y5t-dheoi-qrr3c-dzfri-oc4x3-uqe"}, record {name = "ICPTOWN"; symbol = "ICPTOWN"})'
-  351  dfx identity get-principal
-  352  dfx identity --network ic get-wallet
-  353  dfx identity --network ic balance
-  354  dfx deploy --network ic nft --with-cycles 700000000000
-  355  dfx identity get-principal
-  356  dfx canister --network ic --wallet inrww-miaaa-aaaai-qagta-cai call nft init '(vec {principal "oy7al-zoh7d-fk4pm-o4agp-imod5-j4y5t-dheoi-qrr3c-dzfri-oc4x3-uqe"}, record {name = "ICP TOWN"; symbol = "ICPTOWN"})'
-  357  dfx canister --network ic --wallet my-wallet-canister call nft init '(vec {principal "oy7al-zoh7d-fk4pm-o4agp-imod5-j4y5t-dheoi-qrr3c-dzfri-oc4x3-uqe"}, record {name = "ICP TOWN"; symbol = "ICPTOWN"})'
-  358  dfx identity --network ic get-wallet
-  359  dfx canister --network ic --wallet slb2k-aqaaa-aaaai-qaefq-cai call nft init '(vec {principal "oy7al-zoh7d-fk4pm-o4agp-imod5-j4y5t-dheoi-qrr3c-dzfri-oc4x3-uqe"}, record {name = "ICP TOWN"; symbol = "ICPTOWN"})'
-  360  dfx canister --network ic --no-wallet call nft updateContractOwners '(record {user = principal "bu5da-korq2-4rmym-j645m-7l4w5-5a4xc-5dv4l-uto26-b25i7-bdcql-2qe"; isAuthorized = true})'
-  361  dfx canister --network ic id
-  362  dfx  --network ic canister id nft
-  363  dfx canister --network ic id nft
-  364  dfx canister --network ic --no-wallet call nft getContractInfo
-  365  dfx canister --network ic --no-wallet call nft mint
-  366  [A
-  367  dfx canister --network ic --no-wallet call nft listAssets()
-  368  dfx canister --network ic --no-wallet call nft listAssets
+#!/bin/sh
+random() {
+	tr </dev/urandom -dc A-Za-z0-9 | head -c5
+	echo
+}
 
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+main_interface=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
 
-for file in ./*; dfx canister call nft mint "(record {\
-    index = $(echo $file | sed -E "s/(\.\/art\/)([0-9]+)\.(png)/\2/");\
-    asset = record {\
-        contentType = \"image/$(echo $file | sed -E "s/(\.\/art\/)([0-9]+)\.(png)/\3/")\";\
-        payload = vec {\
-            vec { $(for byte in $(od -v -tuC $file | sed -E "s/[0-9]+//"); echo "$byte;") };\
-        };\
-    }\
-})"
+gen64() {
+	ip64() {
+		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+	}
+	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+}
+install_3proxy() {
+    echo "installing 3proxy"
+    mkdir -p /3proxy
+    cd /3proxy
+    URL="https://github.com/z3APA3A/3proxy/archive/0.9.3.tar.gz"
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-0.9.3
+    make -f Makefile.Linux
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    mv /3proxy/3proxy-0.9.3/bin/3proxy /usr/local/etc/3proxy/bin/
+    wget https://raw.githubusercontent.com/thuongtin/ipv4-ipv6-proxy/master/scripts/3proxy.service-Centos8 --output-document=/3proxy/3proxy-0.9.3/scripts/3proxy.service2
+    cp /3proxy/3proxy-0.9.3/scripts/3proxy.service2 /usr/lib/systemd/system/3proxy.service
+    systemctl link /usr/lib/systemd/system/3proxy.service
+    systemctl daemon-reload
+#    systemctl enable 3proxy
+    echo "* hard nofile 999999" >>  /etc/security/limits.conf
+    echo "* soft nofile 999999" >>  /etc/security/limits.conf
+    echo "net.ipv6.conf.$main_interface.proxy_ndp=1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.all.proxy_ndp=1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
+    echo "net.ipv6.ip_nonlocal_bind = 1" >> /etc/sysctl.conf
+    sysctl -p
+    systemctl stop firewalld
+    systemctl disable firewalld
 
-	for byte in $(od -v -tuC $file | sed -E "s/[0-9]+//")
-	  do 
-	    echo "$byte;"
-	  done
+    cd $WORKDIR
+}
 
+gen_3proxy() {
+    cat <<EOF
+daemon
+maxconn 3000
+nserver 1.1.1.1
+nserver 8.8.4.4
+nserver 2001:4860:4860::8888
+nserver 2001:4860:4860::8844
+nscache 65536
+timeouts 1 5 30 60 180 1800 15 60
+setgid 65535
+setuid 65535
+stacksize 6291456 
+flush
+auth strong
 
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
-	$(dfx canister call nft mint "(
-			record {\
-				contentType = \"image/$(echo $file | sed -E "s/(\.\/art\/)([0-9]+)\.(png)/\3/")\";\
-				payload = vec {\
-            		vec {\
-					for byte in $(od -v -tuC $file | sed -E "s/[0-9]+//")
-					  do 
-						echo "$byte;"
-					  done
-					};\
-				};\
-    		})\
-	)
+$(awk -F "/" '{print "auth strong\n" \
+"allow " $1 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"flush\n"}' ${WORKDATA})
+EOF
+}
 
-dfx canister install nft --argument="(\"ICPT NFT\", \"ICPT\", \"ICP Town NFT collection\", principal \"oy7al-zoh7d-fk4pm-o4agp-imod5-j4y5t-dheoi-qrr3c-dzfri-oc4x3-uqe\")"
+gen_proxy_file_for_user() {
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
+EOF
+}
 
-eval dfx canister --no-wallet install nft --argument="'(\"ICPT NFT\", \"ICPT\", \"ICPT\", \"ICP Town NFT collection\", principal \"$(dfx identity get-principal)\")'"
+upload_proxy() {
+    cd $WORKDIR
+    zip proxy.zip proxy.txt
+    URL=$(curl -F "file=@proxy.zip" https://beta.apileak.com/api/proxy --ssl-no-revoke)
 
+    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+    echo "Check imported at: https://beta.apileak.com/manage/proxy"
+
+}
+gen_data() {
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "$(random)/$(random)/$IP4/$port/$(gen64 $IP6)"
+    done
+}
+
+gen_iptables() {
+    cat <<EOF
+    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+EOF
+}
+
+gen_ifconfig() {
+    cat <<EOF
+$(awk -F "/" '{print "ifconfig '$main_interface' inet6 add " $5 "/56"}' ${WORKDATA})
+EOF
+}
+echo "installing apps"
+yum -y install gcc net-tools bsdtar zip make >/dev/null
+
+install_3proxy
+
+echo "working folder = /home/proxy-installer"
+WORKDIR="/home/proxy-installer"
+WORKDATA="${WORKDIR}/data.txt"
+mkdir $WORKDIR && cd $_
+
+IP4=$(curl -4 -s icanhazip.com)
+IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+
+echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
+
+FIRST_PORT=10000
+LAST_PORT=13000
+
+gen_data >$WORKDIR/data.txt
+gen_iptables >$WORKDIR/boot_iptables.sh
+gen_ifconfig >$WORKDIR/boot_ifconfig.sh
+echo NM_CONTROLLED="no" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
+chmod +x $WORKDIR/boot_*.sh /etc/rc.local
+
+gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
+
+cat >>/etc/rc.local <<EOF
+systemctl start NetworkManager.service
+# ifup ${main_interface}
+bash ${WORKDIR}/boot_iptables.sh
+bash ${WORKDIR}/boot_ifconfig.sh
+ulimit -n 65535
+/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg &
+EOF
+
+bash /etc/rc.local
+
+gen_proxy_file_for_user
+
+upload_proxy
